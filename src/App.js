27e2fc09b9b6e006ae81d334f516707c9a3f83b6
Jacob Carver <jacxbcarver@gmail.com>
2020-05-20 23:11:51 -0600

    add routes to app.js and update packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+
+import { selectCart } from './features/cartManager';
+import { selectResults } from './features/storeManager';
+import { useSelector } from 'react-redux';
+
 import './scss/app.scss';
 
 // Layout
@@ -11,13 +16,15 @@ import Store from './pages/Store';
 import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
+	var cart = useSelector(selectCart).cart;
+	var results = useSelector(selectResults).results;
 	return (
 		<div className="App">
 			<Navigation />
 			<Switch>
 				<Route path={process.env.PUBLIC_URL + '/'} component={Home} exact />
-				<Route path={process.env.PUBLIC_URL + '/store'} component={Store} exact />
-				<Route path={process.env.PUBLIC_URL + '/cart'} component={ShoppingCart} exact />
+				<Route path={process.env.PUBLIC_URL + '/store'} render={() => <Store cart={cart} results={results} />} exact />
+				<Route path={process.env.PUBLIC_URL + '/cart'} render={() => <ShoppingCart cart={cart} />} exact />
 			</Switch>
 		</div>
 	);
