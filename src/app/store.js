c0d51e0e928f43690512c9001944672a6b6705a4
Jacob Carver <jacxbcarver@gmail.com>
2020-05-20 23:11:05 -0600

    add reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '../features/cartManager';
+import storeReducer from '../features/storeManager';
 
 export default configureStore({
 	reducer: {
-		cart: cartReducer
+		cart: cartReducer,
+		results: storeReducer
 	},
 });
