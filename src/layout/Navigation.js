61e82fe0b41ee5acd0fcfc05142876d08ee44828
Jacob Carver <jacxbcarver@gmail.com>
2020-06-04 19:32:18 -0600

    update layout

--- src/layout/Navigation.js
+++ src/layout/Navigation.js
@@ -1,45 +1,128 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectCart } from '../features/cartManager';
+import { TweenMax, Power1 } from 'gsap';
 
 // Image
 import close from '../media/close.svg';
 import cartIcon from '../media/cart.svg';
+import logo from '../media/logo.svg';
 
 export default function Navigation() {
-	const [isOpen, toggle] = useState(false);
+	var [isOpen, toggle] = useState(false);
 	var cart = useSelector(selectCart).cart;
-	const [cartNumber, setNumber] = useState(cart.length);
+	var [cartNumber, setNumber] = useState(cart.length);
+
+	var menu = useRef(null);
+	var line = useRef(null);
+	var links = useRef([]);
+	var socials = useRef([]);
+
 	useEffect(() => {
 		setNumber(cart.length);
 	}, [cart]);
+	useEffect(() => {
+		function scroll(e) {
+			e.preventDefault();
+			if (isOpen) {
+				toggle(false);
+			}
+		}
+		if (isOpen) {
+			// opening menu
+			TweenMax.to(menu, 0.5, {
+				duration: 0.5,
+				left: 0,
+				ease: Power1.easeInOut
+			})
+			// links when opening menu
+			TweenMax.staggerTo(links.current, 0.5, {
+				duration: 0.5,
+				opacity: 1,
+				left: 0,
+				ease: Power1.easeInOut
+			}, 0.1);
+			// line when opening menu
+			TweenMax.to(line, 0.5, {
+				duration: 0.5,
+				opacity: 1,
+				height: '70px',
+				ease: Power1.easeInOut
+			});
+			// icons when opening menu
+			TweenMax.staggerTo(socials.current, 0.25, {
+				duration: 0.5,
+				opacity: 1,
+				ease: Power1.easeInOut,
+				top: 0
+			}, 0.1);
+			// if menu is open, close on scroll
+			window.addEventListener('scroll', scroll);
+		} else {
+			// closing menu
+			TweenMax.to(menu, 0.5, {
+				left: -300,
+				ease: Power1.easeInOut
+			});
+			// links when closing menu
+			TweenMax.staggerTo(links.current, 0.5, {
+				opacity: 0,
+				left: -25,
+				ease: Power1.easeInOut
+			}, 0.2);
+			// line when closing menu
+			TweenMax.to(line, 0.5, {
+				height: '1px',
+				opacity: 0,
+				ease: Power1.easeInOut
+			});
+			// icons when closing menu
+			TweenMax.staggerTo(socials.current, 0.5, {
+				opacity: 0,
+				top: 15,
+				ease: Power1.easeInOut
+			});
+		}
+		return () => {
+			window.removeEventListener('scroll', scroll);
+		}
+	}, [isOpen]);
 	return (
 		<nav>
+			{/* Top Nav */}
 			<div className="container-fluid">
 				<div className="toggler" onClick={() => toggle(!isOpen)}>
 					<div className="bar top-bar"></div>
 					<div className="bar middle-bar"></div>
 					<div className="bar bottom-bar"></div>
 				</div>
-				<div className="genders">
-					<Link to="/store">Men</Link>
-					<div className="divider"></div>
-					<Link to="/store">Women</Link>
-				</div>
+				<Link to="/store" className="logo">
+					<img src={logo} alt="Shoe Store" />
+				</Link>
 				<Link to="/cart" className="cart">
 					<img src={cartIcon} alt="Cart" />
 					<span>{cartNumber}</span>
 				</Link>
 			</div>
-			<div id="menu" className={isOpen ? 'open' : ''}>
-				<div className="close" onClick={() => toggle(!isOpen)}>
+			{/* Menu */}
+			<div ref={el => menu = el} id="menu">
+				<div className="close-menu" onClick={() => toggle(!isOpen)}>
 					<img src={close} alt="Close Menu" />
 				</div>
 				<div className="links">
-					<Link onClick={() => toggle(false)} className={`${window.location.pathname === '/' ? 'active' : ''}`} to="/">Home</Link>
-					<Link onClick={() => toggle(false)} className={`${window.location.pathname === '/store' ? 'active' : ''}`} to="/store">Store</Link>
-					<Link onClick={() => toggle(false)} className={`${window.location.pathname === '/cart' ? 'active' : ''}`} to="/cart">Cart ({cartNumber})</Link>
+					<Link ref={el => links.current[0] = el} onClick={() => toggle(false)} className={`${window.location.pathname === '/' ? 'active' : ''}`} to="/">Home</Link>
+					<Link ref={el => links.current[1] = el} onClick={() => toggle(false)} className={`${window.location.pathname === '/store' ? 'active' : ''}`} to="/store">Store</Link>
+					<Link ref={el => links.current[2] = el} onClick={() => toggle(false)} className={`${window.location.pathname === '/cart' ? 'active' : ''}`} to="/cart">Cart ({cartNumber})</Link>
+				</div>
+				<div className="socials">
+					<div className="icons">
+						<a href="/" ref={el => socials.current[3] = el}><i className="social fab fa-facebook-f"></i></a>
+						<a href="/" ref={el => socials.current[2] = el}><i className="social fab fa-twitter"></i></a>
+						<a href="/" ref={el => socials.current[1] = el}><i className="social fab fa-snapchat-ghost"></i></a>
+						<a href="/" ref={el => socials.current[0] = el}><i className="social fab fa-instagram"></i></a>
+					</div>
+					<div className="line" ref={el => line = el}></div>
 				</div>
 			</div>
 		</nav>
