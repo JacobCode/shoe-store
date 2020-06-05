fdd1d83d8e86102655f87712c37d80c25e1aa841
Jacob Carver <jacxbcarver@gmail.com>
2020-06-04 19:34:46 -0600

    update cartManager

--- src/features/cartManager.js
+++ src/features/cartManager.js
@@ -1,35 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = () => {
+	try {
+		return JSON.parse(localStorage.shoeCart);
+	} catch {
+		return []
+	}
+}
+
+const saveToStorage = (key, value) => {
+	try {
+		localStorage.setItem(key, JSON.stringify(value));
+	} catch {
+		return;
+	}
+}
+
 export const cartManager = createSlice({
 	name: 'cart',
 	initialState: {
-		cart: [
-			{
-				brand: "Adidas",
-				color: "Blue",
-				gender: "Men",
-				img_url: "https://www.flightclub.com/media/catalog/product/cache/1/image/1600x1140/9df78eab33525d08d6e5fb8d27136e95/8/0/804067_01.jpg",
-				name: "PW HU HOLI NMD MC PEACE",
-				price: 550,
-				_id: "5ec0cf256439500631e62446"
-			},
-			{
-				brand: "Adidas",
-				color: "Grey",
-				gender: "Men",
-				img_url: "https://www.flightclub.com/media/catalog/product/cache/1/image/1600x1140/9df78eab33525d08d6e5fb8d27136e95/8/0/805220_01.jpg",
-				name: "YEEZY BOOST 350 V2",
-				price: 325,
-				_id: "5ec0cf256439500631e62447"
-			}
-		]
+		cart: initialState()
 	},
 	reducers: {
 		addToCart: (state, action) => {
 			state.cart = [...state.cart, action.payload];
+			saveToStorage('shoeCart', state.cart);
 		},
 		removeFromCart: (state, action) => {
 			state.cart = [...state.cart.filter((item) => item._id !== action.payload)];
+			saveToStorage('shoeCart', state.cart);
 		}
 	},
 });
